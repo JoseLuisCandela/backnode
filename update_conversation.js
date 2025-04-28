f80be8ebb852997b328d17f0ecce61f2c4ab78fd
JoseLuisCandela <jlcandela@itpfp.com>
2025-04-28 11:25:25 +0200

    update conversation2

--- update_conversation.js
+++ update_conversation.js
@@ -8,7 +8,7 @@ const SUPABASE_URL = 'https://jhutdencubufyjuvtnwx.supabase.co';
 const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodXRkZW5jdWJ1ZnlqdXZ0bnd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMTM5NjcsImV4cCI6MjA2MDg4OTk2N30.x2poq7U5ZlevM_6pxcT0lJfvGaD2XJ5AY-4xpXMWIP0'; // reemplaza con tu clave real
 
 
-router.patch('/update-conversation', async (req, res) => {
+router.post('/update-conversation', async (req, res) => {
   const { id, messages } = req.body;
 
   if (!id || !messages) {
