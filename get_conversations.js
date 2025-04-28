16de6ec565f698f290d1f9e1eef85a56955f995f
JoseLuisCandela <jlcandela@itpfp.com>
2025-04-28 10:29:19 +0200

    get conversation1

--- get_conversations.js
+++ get_conversations.js
@@ -1,12 +1,8 @@
-import express from "express";
 import axios from "axios";
-
-const router = express.Router();
 
 const SUPABASE_URL = "https://jhutdencubufyjuvtnwx.supabase.co";
 const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpodXRkZW5jdWJ1ZnlqdXZ0bnd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMTM5NjcsImV4cCI6MjA2MDg4OTk2N30.x2poq7U5ZlevM_6pxcT0lJfvGaD2XJ5AY-4xpXMWIP0'; // poné tu key
-
-router.get('/get', async (req, res) => {
+const getConversationsHandler = async (req, res) => {
   const { userId } = req.query;
 
   if (!userId) {
@@ -35,7 +31,7 @@ router.get('/get', async (req, res) => {
     console.error(error.response?.data || error.message);
     res.status(500).json({ success: false, error: 'Error al obtener conversaciones' });
   }
-});
+};
 
 function safeParse(str) {
   try {
@@ -45,4 +41,4 @@ function safeParse(str) {
   }
 }
 
-export default router;
+export default getConversationsHandler;
